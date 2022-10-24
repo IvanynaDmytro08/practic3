33e809e9facef56db4d4064b7ca66cf78c79505f
Ivanyna Dmytro <dimaivanina@MacBook-Pro-Dima.local>
2022-10-24 12:59:16 +0300

    First work

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
+
+class Mystring{
+
+private:
+
+    std::string m_string;
+
+public:
+
+    Mystring(const std::string line = " "):m_string(line) {
+    }
+
+    std::string operator()(int index1, int index2){
+
+        std::string rezult;
+
+        for (int i = 0; i < index2; i++) {
+           rezult += m_string[index1 + i];
+        }
+        return rezult;
+    }
+
+};
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    Mystring string("Hello, World!");
+    std::cout << string (7, 6);
+
 }
