35e7258f495c49c636bf901b1a719e156dee4afc
Ivanyna Dmytro <dimaivanina@MacBook-Pro-Dima.local>
2022-10-24 12:11:51 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/WorkPractic3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WorkPractic3.dir/main.cpp.o"
+  "WorkPractic3"
+  "WorkPractic3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WorkPractic3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/WorkPractic3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for WorkPractic3.
+# This may be replaced when dependencies are built.
